5c8fa1d1e31678f0b8add208996a6d44385f46d0
Marco <marco@poop.nl>
2024-12-30 15:19:58 +0100

    run dehydrated upon reprovisioning

--- copy/var/zoneinit/includes/60-dehydrated.sh
+++ copy/var/zoneinit/includes/60-dehydrated.sh
@@ -3,3 +3,6 @@
 test -d /srv/mail/ssl/dehydrated/accounts || dehydrated --register --accept-terms
 
 echo '43 3 * * 0 /opt/local/bin/dehydrated -c -g' >> /etc/cron.d/crontabs/root
+
+# Run once, but ignore failures
+/opt/local/bin/dehydrated/dehydrated -c -g || true
