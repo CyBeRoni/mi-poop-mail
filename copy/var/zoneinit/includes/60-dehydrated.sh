eb81738e34cd617e0fe534305d4de6f6864e81d1
Marco <marco@poop.nl>
2024-12-30 16:52:44 +0100

    fix path

--- copy/var/zoneinit/includes/60-dehydrated.sh
+++ copy/var/zoneinit/includes/60-dehydrated.sh
@@ -5,4 +5,4 @@ test -d /srv/mail/ssl/dehydrated/accounts || dehydrated --register --accept-term
 echo '43 3 * * 0 /opt/local/bin/dehydrated -c -g' >> /etc/cron.d/crontabs/root
 
 # Run once, but ignore failures
-/opt/local/bin/dehydrated/dehydrated -c -g || true
+/opt/local/bin/dehydrated -c -g || true
