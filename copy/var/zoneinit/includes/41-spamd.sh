bbeca59951909c48f09ed0cddd4b32bd9b3cdbf3
Marco <marco@poop.nl>
2024-12-31 14:37:38 +0100

    Fix spamd reload in cron

--- copy/var/zoneinit/includes/41-spamd.sh
+++ copy/var/zoneinit/includes/41-spamd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # create cronjob for sa-update
-echo '0 5 * * * /opt/local/bin/sa-update && kill -SIGHUP $(cat /var/spamassassin/spamd.pid)' >> /etc/cron.d/crontabs/spamd
+echo '2 37 * * * /opt/local/bin/sa-update && svcadm refresh svc:/network/spamd' >> /etc/cron.d/crontabs/spamd
 
 # enable spamd service
 /usr/sbin/svccfg import /opt/local/lib/svc/manifest/spamd.xml
 /usr/sbin/svcadm enable svc:/network/spamd
+
+mkdir /var/run/spamd
